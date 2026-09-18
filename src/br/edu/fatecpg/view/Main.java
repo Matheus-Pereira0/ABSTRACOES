aadb4521f440e0446a5cad5404045f51c9388e12
Lucas Coleti <lucaslingeardi@gmail.com>
2026-09-17 21:30:21 -0300

    Adiciona a classe Cantor

--- src/br/edu/fatecpg/view/Main.java
+++ src/br/edu/fatecpg/view/Main.java
@@ -47,22 +47,22 @@ public class Main {
 		System.out.println("Este estudante está fazendo " + Estudante2.boletim);
 		Estudante2.estudar();
 		
-System.out.println("\n---------------- MÉDICO ----------------");
+		System.out.println("\n---------------- MÉDICO ----------------");
+				
+		Medico Medico1 = new Medico(
+			    "Cardiologia",
+			    "Medicina",
+			    "10 anos",
+			    "CRM123456"
+			);
 		
-Medico Medico1 = new Medico(
-	    "Cardiologia",
-	    "Medicina",
-	    "10 anos",
-	    "CRM123456"
-	);
-
-Medico Medico2 = new Medico(
-	    "Dermatologia",
-	    "Medicina",
-	    "5 anos",
-	    ""
-	);
-		
+		Medico Medico2 = new Medico(
+			    "Dermatologia",
+			    "Medicina",
+			    "5 anos",
+			    ""
+			);
+				
 		System.out.println("\nEsse médico tem especialidade em " + Medico1.especialidade);
 		Medico1.administrarMedicamento();
 		
@@ -85,33 +85,56 @@ Medico Medico2 = new Medico(
 			    "CREA987654"
 			);
 				
-				System.out.println("\nEsse Engenheiro tem função de " + Engenheiro1.funcao);
-				Engenheiro1.planejar();
+		System.out.println("\nEsse Engenheiro tem função de " + Engenheiro1.funcao);
+		Engenheiro1.planejar();
+			
+		System.out.println("\nEsse Engenheiro tem função de " + Engenheiro2.funcao);
+		Engenheiro2.planejar();
 				
-				System.out.println("\nEsse Engenheiro tem função de " + Engenheiro2.funcao);
-				Engenheiro2.planejar();
+		System.out.println("\n---------------- Ator ----------------");
 				
-				System.out.println("\n---------------- Ator ----------------");
-				
-				Ator Ator1 = new Ator(
-					    "DRT123456",
-					    "Artes Cênicas",
-					    "Teatro",
-					    "João"
-					);
+		Ator Ator1 = new Ator(
+			    "DRT123456",
+			    "Artes Cênicas",
+			    "Teatro",
+			    "João"
+			);
 
-					Ator Ator2 = new Ator(
-					    "DRT654321",
-					    "Artes Cênicas",
-					    "Cinema",
-					    "Pedro"
-					);
+		Ator Ator2 = new Ator(
+			    "DRT654321",
+			    "Artes Cênicas",
+			    "Cinema",
+			    "Pedro"
+			);
 						
-						System.out.println("\nEsse Ator tem especialidade em " + Ator1.Especialidade);
-						Ator1.atuar();
+		System.out.println("\nEsse Ator tem especialidade em " + Ator1.Especialidade);
+		Ator1.atuar();
 						
-						System.out.println("\nEsse Ator tem especialidade em " + Ator2.Especialidade);
-						Ator2.atuar();
+		System.out.println("\nEsse Ator tem especialidade em " + Ator2.Especialidade);
+		Ator2.atuar();
+		
+		System.out.println("\n---------------- CANTOR ----------------");
+		
+		Cantor Cantor1 = new Cantor(
+				"Tenor",
+				"Metal",
+				new String[] {"Eagle Fly Free", "Crazy Train"},
+				"Estética Bgseada em um contraste e alta densidade."
+				);
+
+		Cantor Cantor2 = new Cantor(
+				"Meio-Soprano",
+				"City Japanese Pop",
+				new String[] {"Mayonaka no Door / Stay With Me", "Plastic Love", "Neat Na Gojo San-Ji"},
+				"Estética retrô de Tóquio à noite."
+				);
+						
+		System.out.println("\nEsse Cantor tem a voz: " + Cantor1.voz);
+		Cantor1.Cantar();
+						
+		System.out.println("\nEsse Cantor tem a voz: " + Cantor2.voz);
+		Cantor2.Ensaiar();
+		
 	}
 
 }
